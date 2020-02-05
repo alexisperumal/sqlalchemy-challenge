165062fef1badaf1865639fed3e2e7f2a35fdd6c
Alexis Perumal <>
2020-02-05 14:46:47 -0800

    Working on flask app.py

--- app.py
+++ app.py
@@ -1,4 +1,55 @@
+# Alexis Perumal, 2/5/20
+# UCSD Data Science Bootcamp, HW 10
+
 from flask import Flask, jsonify
+
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy import and_
+
+# SQLAlchemy Setup
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+# This function called `calc_temps` will accept start date and end date in the format '%Y-%m-%d' 
+# and return the minimum, average, and maximum temperatures for that range of dates
+def calc_temps(start_date, end_date):
+    """TMIN, TAVG, and TMAX for a list of dates.
+    
+    Args:
+        start_date (string): A date string in the format %Y-%m-%d
+        end_date (string): A date string in the format %Y-%m-%d
+        
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+    
+    return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+
+
+
+
+
+
 
 justice_league_members = [
     {"superhero": "Aquaman", "real_name": "Arthur Curry"},
@@ -10,6 +61,15 @@ justice_league_members = [
     {"superhero": "Wonder Woman", "real_name": "Princess Diana"}
 ]
 
+
+
+
+
+
+
+
+
+
 #################################################
 # Flask Setup
 #################################################
@@ -20,25 +80,46 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
-@app.route("/api/v1.0/justice-league")
-def justice_league():
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Hawaii Climate API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&ltstart&gt<br/>"
+        f"/api/v1.0/&ltstart&gt/&ltend&gt<br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Return Hawaii precipitation data as json"""
+    prcp_data = {}
+    for date, prcp in session.query(Measurement.date, Measurement.prcp).all():
+        prcp_data[date] = prcp
+    return jsonify(prcp_data)
+
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return the station list as json"""
+
+    return jsonify(session.query(Station.station, Station.name, Station.latitude,\
+        Station.longitude, Station.elevation).all())
+
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
     """Return the justice league data as json"""
 
     return jsonify(justice_league_members)
 
 
-@app.route("/")
-def welcome():
-    return (
-        f"Welcome to the Justice League API!<br/>"
-        f"Available Routes:<br/>"
-        f"/api/v1.0/justice-league<br/>"
-        f"/api/v1.0/justice-league/superhero/batman"
-    )
-
-
-@app.route("/api/v1.0/justice-league/real_name/<real_name>")
-def justice_league_by_real_name(real_name):
+@app.route("/api/v1.0/<start>")
+def daily_start(start_date):
     """Fetch the Justice League character whose real_name matches
        the path variable supplied by the user, or a 404 if not."""
 
@@ -52,20 +133,26 @@ def justice_league_by_real_name(real_name):
     return jsonify({"error": f"Character with real_name {real_name} not found."}), 404
 
 
-@app.route("/api/v1.0/justice-league/superhero/<superhero>")
-def justice_league_by_superhero__name(superhero):
-    """Fetch the Justice League character whose superhero matches
+
+@app.route("/api/v1.0/<start>/<end>")
+def daily_start_end(start_date, end_date):
+    """Fetch the Justice League character whose real_name matches
        the path variable supplied by the user, or a 404 if not."""
 
-    canonicalized = superhero.replace(" ", "").lower()
+    canonicalized = real_name.replace(" ", "").lower()
     for character in justice_league_members:
-        search_term = character["superhero"].replace(" ", "").lower()
+        search_term = character["real_name"].replace(" ", "").lower()
 
         if search_term == canonicalized:
             return jsonify(character)
 
-    return jsonify({"error": "Character not found."}), 404
+    return jsonify({"error": f"Character with real_name {real_name} not found."}), 404
 
 
 if __name__ == "__main__":
+    print('Before app.run')
+
+
+
     app.run(debug=True)
+    print('Hi')
